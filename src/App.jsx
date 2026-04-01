179bffe265c7d680a1ae329bde82a7093efd181b
Harika Aleti <haleti@primesoft.net>
2026-04-01 22:43:17 +0530

    setup react app and initial jsx layout

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <h1>Mini Store</h1>
+
+      <div>
+        <h3>iPhone</h3>
+        <p>$1000</p>
+        <button>Add to Cart</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
