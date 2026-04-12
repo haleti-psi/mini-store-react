e597bc8da561eee44bd7a93b4573439a59925511
Harika Aleti <haleti@primesoft.net>
2026-04-12 21:09:07 +0530

    add search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import ProductList from "./components/ProductList";
 import Cart from "./components/Cart";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   const [cart, setCart] = useState([]);
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch("https://dummyjson.com/products")
@@ -24,14 +26,19 @@ function App() {
     setCart((prev) => prev.filter((_, i) => i !== index));
   };
 
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <>
       <h1>Mini Store</h1>
       <h2>Cart: {cart.length}</h2>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <ProductList products={products} addToCart={addToCart} />
+        <ProductList products={filteredProducts} addToCart={addToCart} />
       )}
       <Cart cart={cart} removeFromCart={removeFromCart} />
     </>
